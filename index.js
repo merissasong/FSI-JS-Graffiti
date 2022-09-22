04e785e672015d348df832cb97386fb9041954b3
merissa song <merissa.song@gmail.com>
2022-09-22 18:51:07 -0500

    updated js-graffiti

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.imgAlign = 'upside down'
+}
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+let footer = document.querySelector('.footer')
+footer.style.color = 'pink'
+footer.style.borderStyle = 'solid'
 
 // Select some elements...
